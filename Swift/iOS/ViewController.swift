cf0e9efa48b42c65ed2579ac6d31c15c158eb72e
Satachito <satoru.ogura@me.com>
2016-03-31 08:55:18 +0900

    A

---
+++ Swift/iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by 哲 小倉 on 3/31/16.
+//  Copyright © 2016 Satoru Ogura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
