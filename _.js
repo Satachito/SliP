8d1c8f9fdbfec9c433942d2bf68dfbd0f3f23063
Satoru Ogura <satoru.ogura@me.com>
2022-08-03 02:58:25 +0900

    Break Greek chars

--- _.js
+++ _.js
@@ -1,18 +1,27 @@
 import { NewContext, StringReader, Read, Eval } from './SliP.js'
 
+import * as fs from 'fs'
+const r = new StringReader(
+	fs.readFileSync( '/dev/stdin', 'utf8' ).split( '\n' ).map(
+		_ => {
+			const index = _.indexOf( '//' )
+			return index < 0 ? _ : _.substring( 0, index )
+		}
+	).join( '\n' )
+)
+
 const
 c = NewContext()
 
-import * as fs from 'fs'
-const r = new StringReader( fs.readFileSync( '/dev/stdin', 'utf8' ) )
 while ( true ) {
 	try {
 		const _ = Read( r )
 		if ( !_ ) break
-		console.log( 'READ:', _ )
+		console.log( _.string() )
 		const $ = Eval( c, _ )
-		console.log( 'CONTEXT:', c )
-		console.log( 'VALUE:', $.string() + '\t:', $ )
+	//	console.log( 'CONTEXT:', c )
+	//	console.log( 'VALUE:', $.string() + '\t:', $ )
+		console.log( $.string() )
 	} catch ( e ) {
 		console.log( 'EXCEPTION:', e )
 	}
